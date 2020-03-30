1f2a40219afe0ef25b53dca3621a654b7644fde8
qiuyeyijian <shuishoujun@gmail.com>
2020-03-30 17:39:36 +0800

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// !function ($) {
+//     "use strict";
+//
+//     // Stick the header at top on scroll
+//     $("#header").sticky({
+//         topSpacing: 0,
+//         zIndex: '50'
+//     });
+//
+// }(jQuery);
