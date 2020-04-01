cf5d198ac3c5269e8267aeedc628647646f0154b
qiuyeyijian <shuishoujun@gmail.com>
2020-04-01 16:43:47 +0800

    fix cros

--- api/bing.php
+++ api/bing.php
@@ -1,3 +1,4 @@
 <?php
+header('Access-Control-Allow-Origin:*');
 header("content-type:text/xml;charset=utf-8");
 echo file_get_contents('http://cn.bing.com/HPImageArchive.aspx?idx=0&n=1');   // 从bing获取数据
